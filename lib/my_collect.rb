d39df17f5abb2cad6a39f3cc9eb3240d2db2875a
Eric Kollegger <eric.kollegger@gmail.com>
2017-11-10 16:29:22 +0000

    automatically backed up by learn

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -14,6 +14,19 @@ def my_collect(array)
     newArr
 end
 
+def my_collect(array)
+    newArr = []
+    i = 0
+
+    while i < array.length
+      yield array[i]
+      newArr.push(array[i].split(' ').first)
+      i += 1
+    end
+
+    newArr
+end
+
 # my_collect(languages) do |lang|
 #   lang.upcase
 # end
